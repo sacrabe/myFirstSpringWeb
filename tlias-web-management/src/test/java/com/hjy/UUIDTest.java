b54d038cfdbb61785fce8d7f7632408291a3fdf8
hjyLab <1357826695@qq.com>
2026-01-07 11:37:48 +0800

    more uuid

--- tlias-web-management/src/test/java/com/hjy/UUIDTest.java
+++ tlias-web-management/src/test/java/com/hjy/UUIDTest.java
@@ -11,6 +11,7 @@ public class UUIDTest {
         for (int i = 0; i < 100; i++) {
             System.out.println(UUID.randomUUID());
             System.out.println(UUID.randomUUID());
+            System.out.println(UUID.randomUUID());
         }
 
 
